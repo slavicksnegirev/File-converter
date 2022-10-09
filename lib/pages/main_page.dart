7146b0d383fab2770b48144a1367e71c8a6350c0
Slavick <92212345+slavicksnegirev@users.noreply.github.com>
2022-10-09 22:11:40 +0300

    add platform check

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -25,6 +25,7 @@ class _HomePageState extends State<HomePage> {
 
   var items = [
     'Выберите расширение',
+    'txt',
   ];
 
   void selectFile() async {
@@ -47,89 +48,180 @@ class _HomePageState extends State<HomePage> {
     final isStretched = buttonState == ButtonState.init;
     final isDownloadingDone = buttonState == ButtonState.done;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Конвертер файлов'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            FileDialogWidget(),
-            platformFile != null
-                ? Container(
-                    padding: const EdgeInsets.fromLTRB(40, 0, 40, 20),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          'Выбранный файл',
-                          style: TextStyle(
-                            color: Colors.black26,
-                            fontSize: 15,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Container(
-                            padding: const EdgeInsets.all(8),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: Colors.white,
-                                boxShadow: const [
-                                  BoxShadow(
-                                    color: Colors.black12,
-                                    offset: Offset(0, 1),
-                                    blurRadius: 3,
-                                    spreadRadius: 2,
-                                  )
-                                ]),
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        platformFile!.name,
-                                        style: const TextStyle(
-                                            fontSize: 13, color: Colors.black),
-                                      ),
-                                      const SizedBox(
-                                        height: 5,
-                                      ),
-                                      Text(
-                                        '${(platformFile!.size / 1024).ceil()} KB',
-                                        style: const TextStyle(
-                                            fontSize: 13,
-                                            color: Colors.black26),
-                                      ),
-                                      const SizedBox(
-                                        height: 5,
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            )),
-                      ],
-                    ))
-                : Container(),
-            FileExtensionWidget(),
-            Container(
-              height: 50,
-              padding: const EdgeInsets.fromLTRB(40, 0, 40, 0),
-              child: isStretched
-                  ? ConvertAndDownloadButton()
-                  : LoadingButton(isDownloadingDone),
-            ),
-          ],
+    if (Platform.isFuchsia ||
+        Platform.isMacOS ||
+        Platform.isLinux ||
+        Platform.isWindows) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Конвертер файлов'),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              FileDialogWidget(),
+              platformFile != null
+                  ? Container(
+                      padding: const EdgeInsets.fromLTRB(40, 0, 40, 20),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Выбранный файл',
+                            style: TextStyle(
+                              color: Colors.black26,
+                              fontSize: 15,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                              padding: const EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.white,
+                                  boxShadow: const [
+                                    BoxShadow(
+                                      color: Colors.black12,
+                                      offset: Offset(0, 1),
+                                      blurRadius: 3,
+                                      spreadRadius: 2,
+                                    )
+                                  ]),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          platformFile!.name,
+                                          style: const TextStyle(
+                                              fontSize: 13,
+                                              color: Colors.black),
+                                        ),
+                                        const SizedBox(
+                                          height: 5,
+                                        ),
+                                        Text(
+                                          '${(platformFile!.size / 1024).ceil()} KB',
+                                          style: const TextStyle(
+                                              fontSize: 13,
+                                              color: Colors.black26),
+                                        ),
+                                        const SizedBox(
+                                          height: 5,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              )),
+                        ],
+                      ))
+                  : Container(),
+              FileExtensionWidget(),
+              Container(
+                height: 50,
+                padding: const EdgeInsets.fromLTRB(40, 0, 40, 0),
+                child: isStretched
+                    ? ConvertAndDownloadButton()
+                    : LoadingButton(isDownloadingDone),
+              ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Конвертер файлов'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              FileDialogWidget(),
+              platformFile != null
+                  ? Container(
+                      padding: const EdgeInsets.fromLTRB(40, 0, 40, 20),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Выбранный файл',
+                            style: TextStyle(
+                              color: Colors.black26,
+                              fontSize: 15,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                              padding: const EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.white,
+                                  boxShadow: const [
+                                    BoxShadow(
+                                      color: Colors.black12,
+                                      offset: Offset(0, 1),
+                                      blurRadius: 3,
+                                      spreadRadius: 2,
+                                    )
+                                  ]),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          platformFile!.name,
+                                          style: const TextStyle(
+                                              fontSize: 13,
+                                              color: Colors.black),
+                                        ),
+                                        const SizedBox(
+                                          height: 5,
+                                        ),
+                                        Text(
+                                          '${(platformFile!.size / 1024).ceil()} KB',
+                                          style: const TextStyle(
+                                              fontSize: 13,
+                                              color: Colors.black26),
+                                        ),
+                                        const SizedBox(
+                                          height: 5,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              )),
+                        ],
+                      ))
+                  : Container(),
+              FileExtensionWidget(),
+              Container(
+                height: 50,
+                padding: const EdgeInsets.fromLTRB(40, 0, 40, 0),
+                child: isStretched
+                    ? ConvertAndDownloadButton()
+                    : LoadingButton(isDownloadingDone),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
   }
 
   // ignore: non_constant_identifier_names
